dd621e56233815646b61c0ef10fed79d325f3744
kevinjbutler1994 <kevinjbutler1994@gmail.com>
2024-03-19 08:02:04 -0400

    started cards component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Menu from './components/menu/Menu.jsx';
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./screens/homePage/HomePage.jsx";
 import NavBar from './components/navBar/NavBar.jsx';
+import EventPage from './screens/eventPage/EventPage.jsx';
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <EventPage />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
