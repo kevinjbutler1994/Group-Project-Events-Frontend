4b2713b2179e32e0e2e178b8a24ec1756d7b33ff
Luis Payan <luis.payan1200@gmail.com>
2024-03-19 12:12:23 -0400

    add navbar component help button, menu option for desktop view, add responsive design for mobile, add hover psuedo effect on buttons, add new icons for help button.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,7 +3,19 @@ import "./navBar.css"
 
 function NavBar() {
   return (
-    <div className='navBar'>NavBar</div>
+    <div className='navBar'>
+      <div className='logo'>Logo</div>
+      <div className='searchBar'>Search Bar</div>
+      <div className='navBarMenu'>
+          <div className='computerOptions'>
+            <button className='navFavorites navBarMenuBtn'>Favorites</button>
+            <button className='navSignUp navBarMenuBtn'>SignUp</button>
+            <button className='navLogin navBarMenuBtn'>Login</button>
+          </div>
+          <button className='helpBtn'></button>
+      </div>
+
+    </div>
   )
 }
 
