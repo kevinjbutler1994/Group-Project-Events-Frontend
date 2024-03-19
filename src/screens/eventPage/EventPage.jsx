3a3eba678a7b341bd041f44d8252511e4cd00063
kevinjbutler1994 <kevinjbutler1994@gmail.com>
2024-03-19 12:23:31 -0400

    added event cards

--- src/screens/eventPage/EventPage.jsx
+++ src/screens/eventPage/EventPage.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { boilerPlateData } from "./data.js";
+// import React from "react";
+// import { boilerPlateData } from "./data.js";
 
-export default () => {
-  const generateCardsFromData = collection => {
-    return collection.map(item => {
-      return (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-          <h4>{item.price}</h4>
-          <p>{item.description}</p>
-        </div>
-      );
-    });
-  };
+// export default () => {
+//   const generateCardsFromData = collection => {
+//     return collection.map(item => {
+//       return (
+//         <div key={item.id}>
+//           <h2>{item.name}</h2>
+//           <h4>{item.price}</h4>
+//           <p>{item.description}</p>
+//         </div>
+//       );
+//     });
+//   };
 
-  return <>{generateCardsFromData(boilerPlateData)}</>;
-};
+//   return <>{generateCardsFromData(boilerPlateData)}</>;
+// };
