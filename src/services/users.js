a6f8c8915801da6fb484ff5d011dbbefa3c90d8e
John Lopez <john.lopez3695@gmail.com>
2024-03-20 10:23:30 -0400

    Complete initial sign-up functionality in auth services

--- src/services/users.js
+++ src/services/users.js
@@ -17,6 +17,25 @@ export const login = async (credentials) => {
   }
 };
 
+export const signUp = async (credentials) => {
+  try {
+    console.log("registering", credentials); //TEST DEBUG
+    const response = await api.post("/auth/regist", credentials);
+    localStorage.setItem("token", response.data.token);
+    const user = jwtDecode(response.data.token);
+    return user;
+  } catch (error) {
+    // Log the error for debugging purposes
+    console.error("Register error:", error);
+    // Transform the error into a user-friendly message or custom error object
+    const errorMessage =
+      error.response?.data?.message || "Login failed due to unexpected error";
+
+    // Optionally, throw a new error with the transformed message or handle it differently
+    throw new Error(errorMessage);
+  }
+};
+
 export const signOut = async () => {
   //error handling
   if (localStorage.getItem("token") === null) {
