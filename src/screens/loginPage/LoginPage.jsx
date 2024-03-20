b242285029e9408ae673b1af93c5e61a0330c9ca
josephcoste <josephcoste0@gmail.com>
2024-03-20 10:53:18 -0400

    sign up page form completed needs values and handlesumbit.

--- src/screens/loginPage/LoginPage.jsx
+++ src/screens/loginPage/LoginPage.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+   <h1>Log in</h1>
+   <form onSubmit={handleSubmit}>
+          <input
+         type="text"
+         placeholder="Type your email"
+         onChange={handleChange}
+        />
+
+        <input
+        type="text"
+        placeholder="Type your pasword"
+        onChange={handleChange}
+        />
+      </form>
+    </div>
   )
 }
 
--- src/screens/signUpPage/signUpPage.jsx
+++ src/screens/signUpPage/signUpPage.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import { useState } from 'react'
 
 function RegisterPage() {
+  let [signup, setSignup] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm:"",
+  })
+ 
+  const handleChange = (signup) => {
+    let value = signup.target.value;
+    let name = signup.target.name;
+ 
+    setSignup((prevalue) => {
+      return {
+        ...prevalue,                 
+        [name]: value
+      }
+    })
+  }
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div>
+   <h1>Sign up</h1>
+   <form >
+        <input
+          type="text"
+          placeholder="Type your name "
+          name="name"
+          onChange={handleChange}
+          />
+          <input
+         type="text"
+         placeholder="Type your email"
+         name="email"
+         onChange={handleChange}
+        />
 
-export default RegisterPage
+       <input 
+        type="text"
+        placeholder="Create password"
+        name="password"
+        onChange={handleChange}
+        />
+        <input
+        type="text"
+        placeholder="Confirm pasword"
+        name="confirm"
+        onChange={handleChange}
+        />
+      </form>
+ </div>
+
+
+
+  )
+  }
+
+export default RegisterPage;
