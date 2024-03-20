1274879d365885da2450311bb8ec7106e6eac71d
josephcoste <josephcoste0@gmail.com>
2024-03-20 11:00:19 -0400

    Added forms for the sign up and login page with handle change needs handle submit

--- src/screens/loginPage/LoginPage.jsx
+++ src/screens/loginPage/LoginPage.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
 function LoginPage() {
+  let [login, setLogin] = useState({
+    email: "",
+    password: "",
+   
+  })
+ 
+  const handleChange = (login) => {
+    let value = login.target.value;
+    let name = login.target;
+ 
+    setSignup((prevalue) => {
+      return {
+        ...prevalue,                 
+    
+      }
+    })
+  }
   return (
     <div>
    <h1>Log in</h1>
-   <form onSubmit={handleSubmit}>
+   <form >
           <input
          type="text"
          placeholder="Type your email"
          onChange={handleChange}
         />
-
+        
         <input
         type="text"
         placeholder="Type your pasword"
