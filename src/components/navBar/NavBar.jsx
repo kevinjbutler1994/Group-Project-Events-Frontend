b5bb637e4e9dcee1044aece99b60d618d12d2a63
John Lopez <john.lopez3695@gmail.com>
2024-03-22 09:27:20 -0400

    Refactored code

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import "./navBar.css"
-import { useNavigate } from 'react-router-dom';
-import SearchBar from '../searchBar/SearchBar.jsx';
-function NavBar({showSearch}) {
-  const navigate = useNavigate()
+import React from "react";
+import "./navBar.css";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../searchBar/SearchBar.jsx";
+function NavBar({ showSearch }) {
+  const navigate = useNavigate();
   return (
-    <div className='navBar'>
-      <div className='logo'>Logo</div>
-      <SearchBar showSearch={showSearch}/>
-      <div className='navBarMenu'>
-          <div className='computerOptions'>
-            <button onClick={()=> navigate('/favorite') } className='navFavorites navBarMenuBtn'>Favorites</button>
-            <button  onClick={()=> navigate('/signup') } className='navSignUp navBarMenuBtn'>SignUp</button>
-            <button onClick={()=> navigate('/login') } className='navLogin navBarMenuBtn'>Login</button>
-          </div>
-          <button onClick={()=> navigate('/help') } className='helpBtn'></button>
+    <div className="navBar">
+      <div className="logo">Logo</div>
+
+      <SearchBar showSearch={showSearch} />
+
+      <div className="navBarMenu">
+        <div className="computerOptions">
+          <button
+            onClick={() => navigate("/favorite")}
+            className="navFavorites navBarMenuBtn"
+          >
+            Favorites
+          </button>
+          <button
+            onClick={() => navigate("/signup")}
+            className="navSignUp navBarMenuBtn"
+          >
+            SignUp
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="navLogin navBarMenuBtn"
+          >
+            Login
+          </button>
+        </div>
+        <button onClick={() => navigate("/help")} className="helpBtn"></button>
       </div>
     </div>
-  )
+  );
 }
-export default NavBar
+export default NavBar;
